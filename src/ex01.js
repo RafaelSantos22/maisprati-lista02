f31857e47da643062faef8fc9b973a4cfd09531f
Rafael Santos <rafa_santos33@outlook.com>
2025-06-21 22:19:34 -0300

    Exercício 01 finalizado

---
+++ src/ex01.js
@@ -0,0 +1,19 @@
+/* 1. Validação de Datas 
+    Crie a função ehDataValida(dia, mes, ano) que retorne true se os valores 
+    formarem uma data real (meses de 28–31 dias, ano bissexto para 
+    fevereiro) e false caso contrário. 
+*/
+
+const ehDataValida = (dia, mes, ano) => {
+  const data = new Date(ano, mes - 1, dia);
+  return (
+    data.getFullYear() === ano &&
+    data.getMonth() === mes - 1 &&
+    data.getDate() === dia
+  );
+}
+
+console.log(ehDataValida(29, 2, 2024)); 
+console.log(ehDataValida(29, 2, 2023)); 
+console.log(ehDataValida(31, 4, 2023)); 
+console.log(ehDataValida(15, 10, 2023)); 
